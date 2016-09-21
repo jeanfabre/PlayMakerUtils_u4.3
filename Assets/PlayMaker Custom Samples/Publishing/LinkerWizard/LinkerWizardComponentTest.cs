92fdbfa51ef9ed901f2701fc08a28e6af7a783d4
Jean Fabre <contact@fabrejean.net>
2016-09-21 10:32:16 +0300

    -- update to PlayMaker 1.8.3 -- updated Linkerwizard

---
+++ Assets/PlayMaker Custom Samples/Publishing/LinkerWizard/LinkerWizardComponentTest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinkerWizardComponentTest : MonoBehaviour {
+
+	public void MyMethod()
+	{
+	}
+
+	public static void MyStaticMethod()
+	{
+	}
+}
