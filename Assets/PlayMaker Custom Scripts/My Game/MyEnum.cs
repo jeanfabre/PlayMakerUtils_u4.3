8f6626b5bf8a41c092ce58ecd0adeb7cfa5a55d9
Jean Fabre <contact@fabrejean.net>
2017-01-12 10:52:11 +0300

    Enum Wizard implementation

---
+++ Assets/PlayMaker Custom Scripts/My Game/MyEnum.cs
@@ -0,0 +1,14 @@
+// (c) Copyright HutongGames, LLC 2010-2017. All rights reserved.
+// THIS CONTENT IS AUTOMATICALLY GENERATED.
+// [PLAYMAKER_ENUM]
+// It is recommended to use PlayMaker Enum Creator Wizard to edit this enum definition. But you can perfectly edit this script directly
+
+namespace Net.FabreJean
+{
+	public enum MyEnum
+	{
+		asd,
+		yo,
+		ONE		
+	}
+}
